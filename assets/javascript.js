8f985d7ea25be3d2dfb4e5d2a0cea56e6b84d0a2
Timothy Wewengkang <timothy.yeremia.wewengkang@gmail.com>
2025-09-22 19:39:52 +0700

    <feat> add button system in javascript.

--- assets/javascript.js
+++ assets/javascript.js
@@ -3,3 +3,10 @@ const computerScore = 0;
 
 const userScore_span = document.getElementById("user-score");
 const computerScore_span = document.getElementById("computer-score");
+
+const scoreBoard_div = document.querySelector(".scoreboard");
+const result_div = document.querySelector(".result");
+
+const rock_div = document.getElementById("rock");
+const paper_div = document.getElementById("paper");
+const scissors_div = document.getElementById("scissor");
