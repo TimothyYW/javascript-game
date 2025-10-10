a7dfa49d9ab1ed02d1be480c8cbbdff5c420a544
Timothy <timothy.yeremia.wewengkang@gmail.com>
2025-10-10 20:05:42 +0700

    <feat> adjust name: change user-score and computer-score to the same name within index.HTML file, those are human-score and bot-score.

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,8 +1,8 @@
 let userScore = 0;
 let computerScore = 0;
 
-const userScore_span = document.getElementById("user-score");
-const computerScore_span = document.getElementById("computer-score");
+const userScore_span = document.getElementById("human-score");
+const computerScore_span = document.getElementById("bot-score");
 
 const scoreBoard_div = document.querySelector(".scoreboard");
 const result_div = document.querySelector(".result");
